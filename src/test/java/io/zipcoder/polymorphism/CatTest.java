bf84684d43426833b9cfca97f6626df1362ec5f5
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-07-10 14:01:12 -0400

    Added Tests for subclasses

--- src/test/java/io/zipcoder/polymorphism/CatTest.java
+++ src/test/java/io/zipcoder/polymorphism/CatTest.java
@@ -11,4 +11,28 @@ public class CatTest extends TestCase {
         Assert.assertEquals("Meow!",cat.speak());
 
     }
+
+    public void testName(){
+        Cat cat  =new Cat(null,null,null);
+        cat.setName("Biggs");
+        String actual = cat.getName();
+        String expected = "Biggs";
+        Assert.assertEquals(actual, expected);
+    }
+
+    public void testBreed(){
+        Cat cat = new Cat("Tess", 5, "Tuxedo");
+        String actual = cat.getBreed();
+        String expected = "Tuxedo";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    public void testAge() {
+        Cat cat = new Cat("Tess", 5, "Tuxedo");
+        Integer actual = cat.getAge();
+        Integer expected = 5;
+
+        Assert.assertEquals(actual, expected);
+    }
 }
